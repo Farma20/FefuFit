41ae48957825f4619c759807a26814586eaee192
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-03 14:51:12 +1000

    add provide retrofit function in NetworkModule

--- app/src/main/java/com/example/fefufit/DI/NetworkModule.kt
+++ app/src/main/java/com/example/fefufit/DI/NetworkModule.kt
@@ -1,5 +1,20 @@
 package com.example.fefufit.DI
 
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
 class NetworkModule {
 
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://fefufit.dvfu.ru")
+            .client(okHttpClient)
+            .build()
+    }
 }
