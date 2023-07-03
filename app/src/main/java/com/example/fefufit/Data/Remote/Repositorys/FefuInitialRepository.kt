8594483f0107e7c91b6d49aa8986d6f042a43ac1
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-03 17:17:27 +1000

    create FefuInitialRepository in Data Layer

---
+++ app/src/main/java/com/example/fefufit/Data/Remote/Repositorys/FefuInitialRepository.kt
@@ -0,0 +1,26 @@
+package com.example.fefufit.Data.Remote.Repositorys
+
+import com.example.fefufit.Data.Remote.API.FefuFitApi
+import com.example.fefufit.Data.Remote.Models.SingInDataModel
+import com.example.fefufit.Data.Remote.Models.SingUpDataModel
+import com.example.fefufit.Domain.Repositorys.InitializationRepository
+import javax.inject.Inject
+
+class FefuInitialRepository @Inject constructor(private val fefuFitApi: FefuFitApi):InitializationRepository {
+    override suspend fun singIn(singInData: SingInDataModel) {
+        try {
+            fefuFitApi.singIn(singInData)
+        }catch (e:Exception){
+            println(e)
+        }
+    }
+
+    override suspend fun singUp(singUpData: SingUpDataModel) {
+        try {
+            fefuFitApi.singUp(singUpData)
+        }catch (e:Exception){
+            println(e)
+        }
+    }
+
+}
