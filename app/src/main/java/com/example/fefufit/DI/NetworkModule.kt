e3c2832bc32efb3e37163c705fb94b756c6a791e
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-03 15:09:02 +1000

    add provide functions in NetworkModule (API, HttpClient, Interceptors)

--- app/src/main/java/com/example/fefufit/DI/NetworkModule.kt
+++ app/src/main/java/com/example/fefufit/DI/NetworkModule.kt
@@ -1,9 +1,14 @@
 package com.example.fefufit.DI
 
+import com.example.fefufit.Data.Remote.API.FefuFitApi
+import com.example.fefufit.FefuFitApp
 import dagger.Provides
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 class NetworkModule {
@@ -16,5 +21,33 @@ class NetworkModule {
             .baseUrl("https://fefufit.dvfu.ru")
             .client(okHttpClient)
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApi(retrofit: Retrofit): FefuFitApi{
+        return retrofit.create(FefuFitApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(interceptors: ArrayList<Interceptor>):OkHttpClient{
+        val clientBuilder = OkHttpClient.Builder()
+            .followRedirects(false)
+        interceptors.forEach{
+            clientBuilder.addInterceptor(it)
+        }
+        return clientBuilder.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideInterceptors():ArrayList<Interceptor>{
+        val interceptors = arrayListOf<Interceptor>()
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        interceptors.add(loggingInterceptor)
+        return interceptors
     }
 }
