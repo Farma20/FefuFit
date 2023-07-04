8dd9518efd3bdcc72a0adbf962ae90794b3db3f7
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-04 18:05:47 +1000

    create navigation in initialization screens

--- app/src/main/java/com/example/fefufit/Presentation/Initialization/InitializationScreens.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/InitializationScreens.kt
@@ -1,4 +1,23 @@
 package com.example.fefufit.Presentation.Initialization
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.fefufit.Presentation.Initialization.Navigation.InitializationScreensRout
 
+@Composable
+fun InitializationScreens() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = InitializationScreensRout.SplashScreen.route){
+        composable(route = InitializationScreensRout.SplashScreen.route){
 
+        }
+        composable(route = InitializationScreensRout.SingInScreen.route){
+
+        }
+        composable(route = InitializationScreensRout.SingUpScreen.route){
+
+        }
+    }
+}
--- app/src/main/java/com/example/fefufit/Presentation/Initialization/Navigation/InitializationScreensRout.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/Navigation/InitializationScreensRout.kt
@@ -1,6 +1,6 @@
 package com.example.fefufit.Presentation.Initialization.Navigation
 
-sealed class InitializationScreensRout(val rout:String) {
+sealed class InitializationScreensRout(val route:String) {
     object SplashScreen: InitializationScreensRout("splashScreen")
     object SingInScreen: InitializationScreensRout("singInScreen")
     object SingUpScreen: InitializationScreensRout("singUpScreen")
