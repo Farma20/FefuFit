b5c05b90145777b96138ceec3e5f809d16ccbb1f
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-05 13:55:20 +1000

    paintet upp & down app bars

--- app/src/main/java/com/example/fefufit/MainActivity.kt
+++ app/src/main/java/com/example/fefufit/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             FefuFitTheme {
-                // A surface container using the 'background' color from the theme
                 Surface() {
                     SplashScreen()
                 }
--- app/src/main/java/com/example/fefufit/Presentation/SplashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/fefufit/Presentation/SplashScreen/SplashScreen.kt
@@ -7,15 +7,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.fefufit.Presentation.theme.BlueApp
+import com.example.fefufit.Presentation.theme.WhiteApp
 import com.example.fefufit.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun SplashScreen() {
+
+    //painted system controllers
+    val systemUiController = rememberSystemUiController()
+    val barBackground = BlueApp
+
+    //painted system upp & bottom panels
+    SideEffect {
+        systemUiController.setStatusBarColor(color = barBackground)
+        systemUiController.setNavigationBarColor(color = barBackground)
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
