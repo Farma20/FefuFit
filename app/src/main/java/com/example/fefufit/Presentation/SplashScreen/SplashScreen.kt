b440b542de7815a951b9ad6a67844e67a667229b
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-05 12:21:17 +1000

    add constatn theme colors

---
+++ app/src/main/java/com/example/fefufit/Presentation/SplashScreen/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.fefufit.Presentation.SplashScreen
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+    Surface() {
+        
+    }
+}
