29838aea2e962a7a57ea747fef7782a7245b5d07
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-06 15:04:20 +1000

    creati validation logic in SingInScreenViewModel

--- app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInFormEvent.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInFormEvent.kt
@@ -3,6 +3,5 @@ package com.example.fefufit.Presentation.Initialization.SingInScreen
 sealed class SingInFormEvent{
     data class EmailChanged(val email:String):SingInFormEvent()
     data class PasswordChanged(val password:String):SingInFormEvent()
-
     object Submit:SingInFormEvent()
 }
--- app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInScreenViewModel.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInScreenViewModel.kt
@@ -4,16 +4,64 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.fefufit.Domain.Models.ValidationModels.SingInFromState
 import com.example.fefufit.Domain.UseCases.Initial.Validation.SingInValidation.ValidateEmailUseCase
 import com.example.fefufit.Domain.UseCases.Initial.Validation.SingInValidation.ValidatePasswordUseCase
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class SingInScreenViewModel(
     private val validateEmailUseCase: ValidateEmailUseCase = ValidateEmailUseCase(),
     private val validatePasswordUseCase: ValidatePasswordUseCase = ValidatePasswordUseCase()
 ):ViewModel() {
 
-    var state by mutableStateOf(SingInFromState())
+    var inputDataState by mutableStateOf(SingInFromState())
 
+    //a thread for sending notifications to the UI thread
+    private val validationEventChannel = Channel<ValidationEvent>()
+    val validationEvents = validationEventChannel.receiveAsFlow()
 
+    //listener ui input events
+    fun inputDataEvent(event:SingInFormEvent){
+        when(event){
+            is SingInFormEvent.EmailChanged ->{
+                inputDataState = inputDataState.copy(email = event.email)
+            }
+            is SingInFormEvent.PasswordChanged->{
+                inputDataState = inputDataState.copy(password = event.password)
+            }
+            is SingInFormEvent.Submit ->{
+                submitInputData()
+            }
+        }
+    }
+
+    //validate function
+    private fun submitInputData() {
+        val emailResult = validateEmailUseCase(inputDataState.email)
+        val passwordResult = validatePasswordUseCase(inputDataState.password)
+
+        //find validate errors
+        val hasError = listOf(
+            emailResult,
+            passwordResult,
+        ).any{ !it.success}
+
+        if (hasError){
+            inputDataState = inputDataState.copy(
+                emailError = emailResult.errorMessage,
+                passwordError = passwordResult.errorMessage
+            )
+            return
+        }
+        viewModelScope.launch {
+            validationEventChannel.send(ValidationEvent.Success)
+        }
+    }
+
+    sealed class ValidationEvent{
+        object Success:ValidationEvent()
+    }
 }
