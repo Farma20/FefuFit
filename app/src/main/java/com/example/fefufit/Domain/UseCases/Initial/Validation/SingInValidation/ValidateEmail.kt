deab36e93c177bdb4c5faf2be3fd8ed4c86bb6f9
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-06 14:18:41 +1000

    Create ValidateEmailUseCase

--- app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingInValidation/ValidateEmail.kt
+++ app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingInValidation/ValidateEmail.kt
@@ -1,10 +1,25 @@
 package com.example.fefufit.Domain.UseCases.Initial.Validation.SingInValidation
 
+import android.util.Patterns
+import com.example.fefufit.Domain.Models.ValidationModels.ValidationResult
+
 class ValidateEmail {
 
-    operator fun invoke(email:String){
-        if (email.isBlank()){
-
+    operator fun invoke(email: String): ValidationResult {
+        if (email.isBlank()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Поле не должно быть пустым"
+            )
         }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Некорректный почтовый адрес"
+            )
+        }
+        return ValidationResult(
+            success = true
+        )
     }
 }
