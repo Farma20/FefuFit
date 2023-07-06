3d553f93d98540a9772a17a1937f5035a9d35134
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-06 13:29:54 +1000

    installing splash screen in MainActivity

--- app/src/main/java/com/example/fefufit/MainActivity.kt
+++ app/src/main/java/com/example/fefufit/MainActivity.kt
@@ -3,37 +3,24 @@ package com.example.fefufit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.activity.viewModels
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.fefufit.Data.Remote.Models.InitialModels.SingInDataModel
-import com.example.fefufit.Data.Remote.Models.InitialModels.SingUpDataModel
-import com.example.fefufit.Domain.UseCases.Initial.SingInUseCase
-import com.example.fefufit.Domain.UseCases.Initial.SingUpUseCase
-import com.example.fefufit.Presentation.Initialization.SingInScreen.SingInScreen
 import com.example.fefufit.Presentation.Navigation.MainScreens
-import com.example.fefufit.Presentation.SplashScreen.SplashScreen
-import com.example.fefufit.Presentation.theme.BlueApp
 import com.example.fefufit.Presentation.theme.FefuFitTheme
-import com.example.fefufit.Utils.Resource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel:MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         //installing splashScreen
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepVisibleCondition{
+                viewModel.isLoading.value
+            }
+        }
 
         setContent {
             FefuFitTheme {
