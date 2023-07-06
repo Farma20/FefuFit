78104309bc9f109eb7695772b83890accbf06aef
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-06 13:34:44 +1000

    delete splash screen ui and splashScrennViewModel

--- app/src/main/java/com/example/fefufit/Presentation/Navigation/MainScreens.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Navigation/MainScreens.kt
@@ -1,26 +1,15 @@
 package com.example.fefufit.Presentation.Navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.fefufit.Presentation.Initialization.SingInScreen.SingInScreen
-import com.example.fefufit.Presentation.SplashScreen.SplashScreen
 
 @Composable
 fun MainScreens() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = MainScreensRout.SplashScreen.route){
-        composable(route = MainScreensRout.SplashScreen.route){
-            SplashScreen(navController)
-        }
+    NavHost(navController = navController, startDestination = MainScreensRout.SingInScreen.route){
         composable(route = MainScreensRout.SingInScreen.route){
             SingInScreen(navController)
         }
--- app/src/main/java/com/example/fefufit/Presentation/Navigation/MainScreensRout.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Navigation/MainScreensRout.kt
@@ -1,7 +1,6 @@
 package com.example.fefufit.Presentation.Navigation
 
 sealed class MainScreensRout(val route:String) {
-    object SplashScreen: MainScreensRout("splashScreen")
     object SingInScreen: MainScreensRout("singInScreen")
     object SingUpScreen: MainScreensRout("singUpScreen")
 }
