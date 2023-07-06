1e8abbaa233c4113dd537492bb48a27bebffa5cd
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-06 14:40:44 +1000

    create SingInFromEvent sealed class  in SingInScreen package

---
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInFormEvent.kt
@@ -0,0 +1,8 @@
+package com.example.fefufit.Presentation.Initialization.SingInScreen
+
+sealed class SingInFormEvent{
+    data class EmailChanged(val email:String):SingInFormEvent()
+    data class PasswordChanged(val password:String):SingInFormEvent()
+
+    object Submit:SingInFormEvent()
+}
--- app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInScreenViewModel.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingInScreen/SingInScreenViewModel.kt
@@ -14,4 +14,6 @@ class SingInScreenViewModel(
 ):ViewModel() {
 
     var state by mutableStateOf(SingInFromState())
+
+
 }
