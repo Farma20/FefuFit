fd54cbc122419763fef495b5295ce7a5c6f71950
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-06 14:20:37 +1000

    create ValidatePasswordUseCase

---
+++ app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingInValidation/ValidateEmailUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.fefufit.Domain.UseCases.Initial.Validation.SingInValidation
+
+import android.util.Patterns
+import com.example.fefufit.Domain.Models.ValidationModels.ValidationResult
+
+class ValidateEmailUseCase {
+
+    operator fun invoke(email: String): ValidationResult {
+        if (email.isBlank()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Поле не должно быть пустым"
+            )
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Некорректный почтовый адрес"
+            )
+        }
+        return ValidationResult(
+            success = true
+        )
+    }
+}
--- app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingInValidation/ValidatePasswordUseCase.kt
+++ app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingInValidation/ValidatePasswordUseCase.kt
@@ -3,19 +3,13 @@ package com.example.fefufit.Domain.UseCases.Initial.Validation.SingInValidation
 import android.util.Patterns
 import com.example.fefufit.Domain.Models.ValidationModels.ValidationResult
 
-class ValidateEmail {
+class ValidatePasswordUseCase {
 
-    operator fun invoke(email: String): ValidationResult {
-        if (email.isBlank()) {
+    operator fun invoke(password: String): ValidationResult {
+        if (password.isBlank()) {
             return ValidationResult(
                 success = false,
                 errorMessage = "Поле не должно быть пустым"
-            )
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            return ValidationResult(
-                success = false,
-                errorMessage = "Некорректный почтовый адрес"
             )
         }
         return ValidationResult(
