8857a0d2d15f9039c1f9e166794ea464c6aad231
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-10 18:32:02 +1000

    create personal data input form in SingInScreen

--- app/src/main/java/com/example/fefufit/Presentation/Initialization/SingUpScreen/SingUpScreen.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingUpScreen/SingUpScreen.kt
@@ -4,36 +4,47 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fefufit.Presentation.theme.BlackApp
 import com.example.fefufit.Presentation.theme.BlueApp
+import com.example.fefufit.Presentation.theme.RedErrorApp
+import com.example.fefufit.Presentation.theme.SecondaryTextApp
+import com.example.fefufit.Presentation.theme.WhiteApp
 import com.example.fefufit.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -48,22 +59,30 @@ fun SingUpScreen() {
             UppBar(modifier = Modifier)
         }
     ) {scaffoldPadding->
-        Column(
+        Surface(
             modifier = Modifier
-                .fillMaxWidth()
-                .horizontalScroll(scrollState),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.height(scaffoldPadding.calculateTopPadding()))
-            RegistrationPager(Modifier.fillMaxWidth())
-            Spacer(modifier = Modifier.height(15.dp))
-            Text(
-                text = "Личные данные",
-                fontSize = 22.sp,
-                fontWeight = FontWeight(600),
-                color = BlackApp,
-                textAlign = TextAlign.Center,
-            )
+                .fillMaxSize()
+                .verticalScroll(scrollState),
+            color = WhiteApp
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(scaffoldPadding.calculateTopPadding()))
+                RegistrationPager(Modifier)
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = "Личные данные",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(600),
+                    color = BlackApp,
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                PersonalDataInputForm()
+            }
         }
     }
 }
@@ -131,4 +150,214 @@ private fun RegistrationPager(modifier: Modifier){
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonalDataInputForm() {
+    Row(
+        modifier = Modifier.fillMaxWidth(0.86f),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(
+            text = "Фамилия",
+            fontSize = 14.sp,
+            color = SecondaryTextApp
+        )
+        Text(
+            text = " *",
+            color = RedErrorApp
+        )
+    }
+    Spacer(modifier = Modifier.height(6.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(0.90f),
+        shape = RoundedCornerShape(13.dp),
+        value = "",
+        onValueChange = {
+//            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueApp
+        )
+    )
+    Spacer(modifier = Modifier.height(14.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(0.86f),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(
+            text = "Имя",
+            fontSize = 14.sp,
+            color = SecondaryTextApp
+        )
+        Text(
+            text = " *",
+            color = RedErrorApp,
+        )
+    }
+    Spacer(modifier = Modifier.height(6.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(0.90f),
+        shape = RoundedCornerShape(13.dp),
+        value = "",
+        onValueChange = {
+//            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueApp
+        )
+    )
+    Spacer(modifier = Modifier.height(14.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(0.86f),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(
+            text = "Отчество",
+            fontSize = 14.sp,
+            color = SecondaryTextApp
+        )
+    }
+    Spacer(modifier = Modifier.height(6.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(0.90f),
+        shape = RoundedCornerShape(13.dp),
+        value = "",
+        onValueChange = {
+//            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueApp
+        )
+    )
+    Spacer(modifier = Modifier.height(14.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(0.86f),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(
+            text = "Пол",
+            fontSize = 14.sp,
+            color = SecondaryTextApp
+        )
+        Text(
+            text = " *",
+            color = RedErrorApp,
+        )
+    }
+    Spacer(modifier = Modifier.height(6.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(0.90f),
+        shape = RoundedCornerShape(13.dp),
+        value = "",
+        onValueChange = {
+//            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueApp
+        )
+    )
+
+    Spacer(modifier = Modifier.height(14.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(0.86f),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(
+            text = "Дата рождения",
+            fontSize = 14.sp,
+            color = SecondaryTextApp
+        )
+        Text(
+            text = " *",
+            color = RedErrorApp,
+        )
+    }
+    Spacer(modifier = Modifier.height(6.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(0.90f),
+        shape = RoundedCornerShape(13.dp),
+        value = "",
+        onValueChange = {
+//            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueApp
+        )
+    )
+    Spacer(modifier = Modifier.height(14.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(0.86f),
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Text(
+            text = "Статус",
+            fontSize = 14.sp,
+            color = SecondaryTextApp
+        )
+        Text(
+            text = " *",
+            color = RedErrorApp,
+        )
+    }
+    Spacer(modifier = Modifier.height(6.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(0.90f),
+        shape = RoundedCornerShape(13.dp),
+        value = "",
+        onValueChange = {
+//            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueApp
+        )
+    )
+
+//    if (inputDataState.emailError != null) {
+//        Text(
+//            modifier = Modifier
+//                .fillMaxWidth(0.81f),
+//            text = inputDataState.emailError,
+//            fontSize = 14.sp,
+//            color = MaterialTheme.colorScheme.error,
+//            textAlign = TextAlign.End
+//        )
+//    }
 }
