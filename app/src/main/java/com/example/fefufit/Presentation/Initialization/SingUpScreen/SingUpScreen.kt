e5e80ea14fdbbd5db059ce724aabe05a1641df0f
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-10 20:02:40 +1000

    add gender drop down menu in SingInScreen

--- app/src/main/java/com/example/fefufit/Presentation/Initialization/SingUpScreen/SingUpScreen.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingUpScreen/SingUpScreen.kt
@@ -3,7 +3,6 @@ package com.example.fefufit.Presentation.Initialization.SingUpScreen
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -23,18 +21,26 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,8 +51,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.fefufit.Presentation.Initialization.SingInScreen.SingInFormEvent
-import com.example.fefufit.Presentation.Initialization.SingInScreen.SingInScreenViewModel
 import com.example.fefufit.Presentation.theme.BlackApp
 import com.example.fefufit.Presentation.theme.BlueApp
 import com.example.fefufit.Presentation.theme.RedErrorApp
@@ -182,6 +186,13 @@ private fun RegistrationPager(modifier: Modifier){
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PersonalDataInputForm() {
+
+    //gender drop down menu variables
+    var genderDropDown by remember { mutableStateOf(false) }
+    val listGenders = listOf("Мужчина", "Женщина")
+    var genderItems by remember { mutableStateOf(listGenders) }
+    var selectedGenderItem by remember { mutableStateOf("") }
+
     Row(
         modifier = Modifier.fillMaxWidth(0.86f),
         horizontalArrangement = Arrangement.Start,
@@ -292,23 +303,60 @@ fun PersonalDataInputForm() {
         )
     }
     Spacer(modifier = Modifier.height(6.dp))
-    OutlinedTextField(
-        modifier = Modifier
-            .fillMaxWidth(0.90f),
-        shape = RoundedCornerShape(13.dp),
-        value = "",
-        onValueChange = {
+    ExposedDropdownMenuBox(
+        expanded = genderDropDown,
+        onExpandedChange = {genderDropDown = !genderDropDown}
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(0.90f)
+                .menuAnchor(),
+            readOnly = true,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = genderDropDown
+                )
+            },
+            shape = RoundedCornerShape(13.dp),
+            value = selectedGenderItem,
+            onValueChange = {
 //            viewModel.inputDataEvent(SingInFormEvent.EmailChanged(it))
-        },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text
-        ),
-        singleLine = true,
-        textStyle = TextStyle(fontSize = 16.sp),
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = BlueApp
+            },
+            placeholder = {
+                Text(
+                    text = "Не выбрано",
+                    fontSize = 18.sp,
+                    lineHeight = 22.sp,
+                    fontWeight = FontWeight(200),
+                    color = BlackApp,
+                )
+            },
+            singleLine = true,
+            textStyle = TextStyle(fontSize = 16.sp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = BlueApp,
+            )
         )
-    )
+        ExposedDropdownMenu(
+            modifier = Modifier.background(WhiteApp),
+            expanded = genderDropDown,
+            onDismissRequest = { genderDropDown = false }
+        ) {
+            // this is a column scope
+            // all the items are added vertically
+            genderItems.forEach { selectedGender ->
+                // menu item
+                DropdownMenuItem(
+                    modifier = Modifier.background(WhiteApp),
+                    text = { Text(text = selectedGender) },
+                    onClick = {
+                        selectedGenderItem = selectedGender
+                        genderDropDown = false
+                    },
+                )
+            }
+        }
+    }
 
     Spacer(modifier = Modifier.height(14.dp))
     Row(
