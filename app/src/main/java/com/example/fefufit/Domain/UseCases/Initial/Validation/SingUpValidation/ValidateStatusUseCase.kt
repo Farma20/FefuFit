6f9454f08a11d9a45d6ee271912f876afc5ad4a5
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-12 18:34:50 +1000

    create status validate use case

---
+++ app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingUpValidation/ValidateStatusUseCase.kt
@@ -0,0 +1,18 @@
+package com.example.fefufit.Domain.UseCases.Initial.Validation.SingUpValidation
+
+import android.util.Patterns
+import com.example.fefufit.Domain.Models.ValidationModels.ValidationResult
+
+class ValidateStatusUseCase {
+    operator fun invoke(status: String): ValidationResult {
+        if (status.isBlank()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Поле не должно быть пустым"
+            )
+        }
+        return ValidationResult(
+            success = true
+        )
+    }
+}
