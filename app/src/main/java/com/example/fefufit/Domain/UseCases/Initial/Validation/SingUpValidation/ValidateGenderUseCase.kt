e50cbecba6387dd4fe90ca090ba9bbb93b0b0f59
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-12 18:32:14 +1000

    create gender validate use casze

---
+++ app/src/main/java/com/example/fefufit/Domain/UseCases/Initial/Validation/SingUpValidation/ValidateGenderUseCase.kt
@@ -0,0 +1,18 @@
+package com.example.fefufit.Domain.UseCases.Initial.Validation.SingUpValidation
+
+import android.util.Patterns
+import com.example.fefufit.Domain.Models.ValidationModels.ValidationResult
+
+class ValidateGenderUseCase {
+    operator fun invoke(gender: String): ValidationResult {
+        if (gender.isBlank()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Поле не должно быть пустым"
+            )
+        }
+        return ValidationResult(
+            success = true
+        )
+    }
+}
