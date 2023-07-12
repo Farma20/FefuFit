fef4055c9aea3627e4c43c6147e2312cab56e6af
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-12 18:27:29 +1000

    create SingUpFormState data validate class

---
+++ app/src/main/java/com/example/fefufit/Domain/Models/ValidationModels/SingUpFormState.kt
@@ -0,0 +1,16 @@
+package com.example.fefufit.Domain.Models.ValidationModels
+
+data class SingUpFormState(
+    val secondName:String = "",
+    val secondNameError:String? = null,
+    val firstName:String = "",
+    val firstNameError:String? = null,
+    val middleName:String = "",
+    val middleNameError:String? = null,
+    val gender:String = "",
+    val genderError:String? = null,
+    val birthday:String = "",
+    val birthdayError:String? = null,
+    val status:String = "",
+    val statusError:String? = null
+)
