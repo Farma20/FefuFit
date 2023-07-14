6c24945f1b78b8be87b73b46026ee6b4351149cf
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-14 15:40:09 +1000

    add animated input fields chenges

--- app/src/main/java/com/example/fefufit/Presentation/Initialization/SingUpScreen/Navigation/SingUpFieldsScreens.kt
+++ app/src/main/java/com/example/fefufit/Presentation/Initialization/SingUpScreen/Navigation/SingUpFieldsScreens.kt
@@ -1,22 +1,68 @@
 package com.example.fefufit.Presentation.Initialization.SingUpScreen.Navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.fefufit.Presentation.Initialization.SingUpScreen.Elements.SingUpFieldsFirst
 import com.example.fefufit.Presentation.Initialization.SingUpScreen.Elements.SingUpFieldsSecond
 import com.example.fefufit.Presentation.Initialization.SingUpScreen.SingUpScreenViewModel
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SingUpFieldsScreens(viewModel: SingUpScreenViewModel) {
     val navController = rememberAnimatedNavController()
-    NavHost(navController = navController, startDestination = SingUpFieldsScreensRoute.SingUpFieldsFirst.route){
+    AnimatedNavHost(navController = navController, startDestination = SingUpFieldsScreensRoute.SingUpFieldsFirst.route){
         viewModel.getFieldsNavController(navController = navController)
-        composable(route = SingUpFieldsScreensRoute.SingUpFieldsFirst.route){
+        composable(
+            route = SingUpFieldsScreensRoute.SingUpFieldsFirst.route,
+            exitTransition = {
+                slideOutHorizontally (
+                    targetOffsetX = {-1080},
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            },
+            popEnterTransition = {
+                slideInHorizontally (
+                    initialOffsetX = {-1080},
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            },
+        ){
             SingUpFieldsFirst(viewModel = viewModel)
         }
-        composable(route = SingUpFieldsScreensRoute.SingUpFieldsSecond.route){
+        composable(
+            route = SingUpFieldsScreensRoute.SingUpFieldsSecond.route,
+            enterTransition = {
+                slideInHorizontally (
+                    initialOffsetX = {1080},
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            },
+            popExitTransition = {
+                slideOutHorizontally (
+                    targetOffsetX = {1080},
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            },
+        ){
             SingUpFieldsSecond(viewModel = viewModel)
         }
     }
