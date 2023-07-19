d8debcaedc23443c25b7ae52ac9d41a8ffcc1c9b
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-19 17:57:57 +1000

    update dagger hilt version

--- app/src/main/java/com/example/fefufit/Presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/example/fefufit/Presentation/MainActivityViewModel.kt
@@ -2,10 +2,12 @@ package com.example.fefufit.Presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+
 
 class MainActivityViewModel:ViewModel() {
     private val _isLoading = MutableStateFlow(true)
