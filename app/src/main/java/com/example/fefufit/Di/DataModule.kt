089deb4edc3e11f628cf22d59c011dbcf85ebb0a
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-19 16:46:29 +1000

    add data module in Hillt and detete DI_Dagger2 package

--- app/src/main/java/com/example/fefufit/Di/DataModule.kt
+++ app/src/main/java/com/example/fefufit/Di/DataModule.kt
@@ -1,18 +1,20 @@
-package com.example.fefufit.DI_Dagger2
+package com.example.fefufit.Di
 
 import com.example.fefufit.Data.Remote.API.FefuFitApi
 import com.example.fefufit.Data.Remote.Repositorys.FefuInitialRepository
 import com.example.fefufit.Domain.Repositorys.InitializationRepository
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-class DataModule {
-
+@InstallIn(SingletonComponent::class)
+object DataModule {
     @Provides
     @Singleton
-    fun provideInitialRepository(api: FefuFitApi):InitializationRepository{
+    fun provideInitialRepository(api: FefuFitApi): InitializationRepository {
         return FefuInitialRepository(api)
     }
 }
--- app/src/main/java/com/example/fefufit/Di/NetworkModule.kt
+++ app/src/main/java/com/example/fefufit/Di/NetworkModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object NetworkModule {
 
     @Provides
     @Singleton
