e99f0ebe03ed5718f87fdf4aad2f2c68e3db5368
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-24 12:59:03 +1000

    --fix UserMetaData data class

--- app/src/main/java/com/example/fefufit/Data/Internal/DataStore/Entities/AppInternalData.kt
+++ app/src/main/java/com/example/fefufit/Data/Internal/DataStore/Entities/AppInternalData.kt
@@ -3,6 +3,11 @@ package com.example.fefufit.Data.Internal.DataStore.Entities
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class AppInternalData(
+    val userMetaData: UserMetaData = UserMetaData()
+)
+
+@Serializable
 data class UserMetaData(
     val userToken:String? = null,
     val userQrToken: String? = null
