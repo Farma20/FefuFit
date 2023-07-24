ea0b51fb2f23a817313c8d668b6d010846f3234c
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-24 16:50:24 +1000

    create UserDataFefuRepository with observe user token and add UserDataFefuRepository in Hillt

--- app/src/main/java/com/example/fefufit/Data/Remote/Repositorys/UserDataFefuRepository.kt
+++ app/src/main/java/com/example/fefufit/Data/Remote/Repositorys/UserDataFefuRepository.kt
@@ -1,14 +1,33 @@
 package com.example.fefufit.Data.Remote.Repositorys
 
+import com.example.fefufit.Data.Internal.DataStore.DataStoreManager
+import com.example.fefufit.Data.Remote.API.FefuFitApi
 import com.example.fefufit.Data.Remote.Models.UserDataModels.UserInfoDataModel
 import com.example.fefufit.Domain.Repositorys.UserDataRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
 
-class UserDataFefuRepository(): UserDataRepository {
+class UserDataFefuRepository @Inject constructor(
+   private val api: FefuFitApi,
+   private val dataStoreManager: DataStoreManager
+): UserDataRepository {
+
+    private var userToken:String? = null
+
+    init {
+        dataStoreManager.data.onEach {
+            userToken = it.userMetaData.userToken
+        }.launchIn(CoroutineScope(Dispatchers.IO))
+    }
+
     override suspend fun getUserData(): UserInfoDataModel {
-        TODO("Not yet implemented")
+        return api.getUserData(mapOf("token" to userToken!!))
     }
 
     override suspend fun editUserData(): Map<String, String> {
-        TODO("Not yet implemented")
+       return mapOf("don`t_work" to  "don`t_work")
     }
 }
--- app/src/main/java/com/example/fefufit/Di/RepositoryModule.kt
+++ app/src/main/java/com/example/fefufit/Di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.example.fefufit.Di
 
 import com.example.fefufit.Data.Remote.Repositorys.InitialFefuRepository
+import com.example.fefufit.Data.Remote.Repositorys.UserDataFefuRepository
 import com.example.fefufit.Domain.Repositorys.InitializationRepository
+import com.example.fefufit.Domain.Repositorys.UserDataRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ abstract class RepositoryModule {
     abstract fun bindInitialRepository(
         initialRepository: InitialFefuRepository
     ):InitializationRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindUserDataRepository(
+        userDataRepository: UserDataFefuRepository
+    ):UserDataRepository
 }
