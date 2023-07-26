364b5793330684e3c013e7fb8dda0397c53e53d3
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-26 12:45:59 +1000

    create UserShortDataUseCase in Domain layer

--- app/src/main/java/com/example/fefufit/Domain/UseCases/Main/UsersUseCases/UserShortDataUseCase.kt
+++ app/src/main/java/com/example/fefufit/Domain/UseCases/Main/UsersUseCases/UserShortDataUseCase.kt
@@ -1,19 +1,25 @@
 package com.example.fefufit.Domain.UseCases.Main.UsersUseCases
 
 import com.example.fefufit.Data.Remote.Models.UserDataModels.UserShortDataModel
+import com.example.fefufit.Data.Remote.Models.UserDataModels.toShort
 import com.example.fefufit.Domain.Repositorys.UserDataRepository
 import com.example.fefufit.Utils.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class UserShortDataUseCase(
+class UserShortDataUseCase @Inject constructor(
     private val repository: UserDataRepository
 ) {
     operator fun invoke(): Flow<Resource<UserShortDataModel>> = flow {
         try {
             emit(Resource.Loading())
-            val response = repository.getUserData()
-
+            val userData = repository.getUserData()
+            emit(Resource.Success(userData.toShort()))
+        }
+        catch (e:Exception){
+            val errorText = "Ошибка, данные не были получены"
+            emit(Resource.Error(errorText))
         }
     }
 }
