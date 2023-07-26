3774935cfdc7e09e8fce4e744d17d7e51f4e0909
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-26 18:29:33 +1000

    create NearEventSpace in MainMenuScreen

--- app/src/main/java/com/example/fefufit/presentation/main_menu/MainMenuScreen.kt
+++ app/src/main/java/com/example/fefufit/presentation/main_menu/MainMenuScreen.kt
@@ -1,21 +1,16 @@
 package com.example.fefufit.presentation.main_menu
 
 import android.annotation.SuppressLint
-import android.widget.ImageButton
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -24,10 +19,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -53,12 +48,52 @@ fun MainMenuScreen() {
             MainMenuUppBar()
             Spacer(modifier = Modifier.height(26.dp))
             QrCard()
+            Spacer(modifier = Modifier.height(18.dp))
+            NearEventSpace()
         }
     }
 }
 
 @Composable
-fun QrCard() {
+fun NearEventSpace() {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Ближайшее занятие",
+                fontSize = 22.sp,
+                fontWeight = FontWeight(400),
+                color = FefuFitTheme.color.textColor.mainTextColor,
+            )
+            Row(
+                modifier = Modifier
+                    .padding(top = 4.dp)
+                    .clickable {  },
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "Все записи ",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight(400),
+                    color = FefuFitTheme.color.textColor.secondaryTextColor,
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.next_arrows),
+                    contentDescription = "nextArrows",
+                    tint = FefuFitTheme.color.textColor.secondaryTextColor,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun QrCard() {
     Card(
         modifier = Modifier
             .fillMaxWidth(),
