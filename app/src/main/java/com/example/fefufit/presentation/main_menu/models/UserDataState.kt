56c5f2cdd13546f70aaab5922a3c940fb939a275
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-05 12:53:32 +1000

    create UserDataState data class in main_menu package

---
+++ app/src/main/java/com/example/fefufit/presentation/main_menu/models/UserDataState.kt
@@ -0,0 +1,9 @@
+package com.example.fefufit.presentation.main_menu.models
+
+import com.example.fefufit.data.remote.models.user_data_models.UserShortDataModel
+
+data class UserDataState(
+    val isLoading: Boolean = false,
+    val data: UserShortDataModel = UserShortDataModel(),
+    val isError: String? = null
+)
