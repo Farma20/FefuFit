f7eb28438a14546a8a9b96a8f8f0c0ecdf23f5f4
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-05 10:27:37 +1000

    donload pagers dependences in gradle

--- app/src/main/java/com/example/fefufit/presentation/main_menu/MainMenuScreen.kt
+++ app/src/main/java/com/example/fefufit/presentation/main_menu/MainMenuScreen.kt
@@ -1,7 +1,6 @@
 package com.example.fefufit.presentation.main_menu
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -22,16 +21,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -39,42 +36,39 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.IntrinsicMeasurable
 import androidx.compose.ui.layout.IntrinsicMeasureScope
 import androidx.compose.ui.layout.LayoutModifier
 import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.layout.MeasureResult
 import androidx.compose.ui.layout.MeasureScope
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fefufit.R
 import com.example.fefufit.presentation.theme.FefuFitTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalPagerApi::class)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainMenuScreen() {
+
+    val pagerState = rememberPagerState()
+
     Scaffold(
         containerColor = FefuFitTheme.color.mainAppColors.appBackgroundColor,
     ) {
@@ -90,13 +84,13 @@ fun MainMenuScreen() {
             Spacer(modifier = Modifier.height(18.dp))
             NearEventSpace()
             Spacer(modifier = Modifier.height(18.dp))
-            ActiveServicesSpace()
+            ActiveServicesSpace(pagerState)
         }
     }
 }
 
 @Composable
-private fun ActiveServicesSpace() {
+private fun ActiveServicesSpace(pagerState: PagerState) {
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
@@ -113,7 +107,19 @@ private fun ActiveServicesSpace() {
             )
         }
         Spacer(modifier = Modifier.height(10.dp))
-        ActiveServicesCard()
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            HorizontalPager(
+                modifier = Modifier.fillMaxWidth(),
+                count = 4,
+                state = pagerState
+            ) {id->
+                ActiveServicesCard()
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            HorizontalPagerIndicator(pagerState = pagerState)
+        }
     }
 }
 
@@ -207,7 +213,9 @@ fun ActiveServicesCard() {
                         for (i in 1..16){
                             val visit = i in 1..4
                             ServiceCircle(i, visit)
-                            Spacer(modifier = Modifier.width(2.dp).height(34.dp))
+                            Spacer(modifier = Modifier
+                                .width(2.dp)
+                                .height(34.dp))
                         }
                     }
                 }
