1042e414847c60d6048104df0c5b30c2f7dad148
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-09 10:33:15 +1000

    delete initial module, add compose configuration in core module

--- app/src/main/java/com/example/fefufit/presentation/MainActivity.kt
+++ app/src/main/java/com/example/fefufit/presentation/MainActivity.kt
@@ -15,15 +15,10 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.fefufit.data.internal.data_store.DataStoreManager
 import com.example.fefufit.data.internal.data_store.entities.AppInternalData
 import com.example.fefufit.domain.use_cases.main.user_use_cases.UserActiveServiceUseCase
-import com.example.fefufit.presentation.initialization.navigation.InitializationScreens
 import com.example.fefufit.presentation.main_menu.MainMenuScreen
-import com.example.core.theme.FefuFitTheme
-import com.example.fefufit.utils.Resource
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.example.core.theme.FefuFitTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -54,7 +49,7 @@ class MainActivity : ComponentActivity() {
             val userToken = dataStoreManager.data.collectAsState(initial = AppInternalData())
 
             val isDarkTheme = isSystemInDarkTheme()
-            com.example.core.theme.FefuFitTheme(isDarkTheme) {
+            FefuFitTheme(isDarkTheme) {
                 val scope = rememberCoroutineScope()
 
 //                scope.launch {
@@ -79,7 +74,7 @@ class MainActivity : ComponentActivity() {
                 //painted system controllers
                 val systemUiController = rememberSystemUiController()
                 val barBackground =
-                    com.example.core.theme.FefuFitTheme.color.mainAppColors.appBackgroundColor
+                    FefuFitTheme.color.mainAppColors.appBackgroundColor
 
                 //painted system upp & bottom panels
                 SideEffect {
