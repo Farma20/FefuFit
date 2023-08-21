860d6c94f8c8702f78b6544b19d6c29ac2e0e87d
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-21 17:06:05 +1000

    inheritance from featureApiHolder in MainActivityViewModel and provide screens dependences from hilt

--- app/src/main/java/com/example/fefufit/presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/example/fefufit/presentation/MainActivityViewModel.kt
@@ -2,13 +2,23 @@ package com.example.fefufit.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.fefufit.navigation.FeatureApiHolder
+import com.example.main_api.MainPageApi
+import com.example.sing_in_api.SingInApi
+import com.example.sing_up_api.SingUpApi
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
-class MainActivityViewModel:ViewModel() {
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    override val singInScreen: SingInApi,
+    override val singUpScreen: SingUpApi,
+    override val mainPageApi: MainPageApi
+) :ViewModel(), FeatureApiHolder {
     private val _isLoading = MutableStateFlow(true)
     val isLoading = _isLoading.asStateFlow()
 
