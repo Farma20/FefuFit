82fd50c8e60136e04df95cfd2871b7cbd985ba8b
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-28 16:10:22 +1000

    create AdapterMetaDataRepository in app-module

--- app/src/main/java/com/example/fefufit/glue/main_screen/repositories/AdapterMetaDataRepository.kt
+++ app/src/main/java/com/example/fefufit/glue/main_screen/repositories/AdapterMetaDataRepository.kt
@@ -1,4 +1,23 @@
 package com.example.fefufit.glue.main_screen.repositories
 
-class AdapterMetaDataRepository {
+import com.example.data_store.DataStoreRepository
+import com.example.main_impl.domain.repositories.MainMetaDataRepository
+import javax.inject.Inject
+
+class AdapterMetaDataRepository @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository
+):MainMetaDataRepository {
+    override suspend fun getUserTokenMetaData(): String {
+        var token:String? = null
+        dataStoreRepository.getUserMetaData().collect{result->
+            token = result.userMetaData.userToken
+        }
+        if (!token.isNullOrEmpty()){
+            return token!!
+        }
+        else{
+            throw Exception("incorrect token")
+        }
+    }
+
 }
