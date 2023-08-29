98bcfdc21057cef0737e42aab9f490cf31986f8b
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-29 14:11:00 +1000

    creaete exception handler in UserFefuRepository in data:remote module

--- data/remote/src/main/java/com/example/remote/repositories/UserFefuRepository.kt
+++ data/remote/src/main/java/com/example/remote/repositories/UserFefuRepository.kt
@@ -4,6 +4,9 @@ package com.example.fefufit.data.remote.repositories
 import com.example.fefufit.data.remote.api.FefuFitApi
 import com.example.fefufit.data.remote.models.user_data_models.DataUserDataModel
 import com.example.remote.UserDataRepository
+import com.example.remote.utils.toMap
+import org.json.JSONObject
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class UserFefuRepository @Inject constructor(
@@ -11,7 +14,25 @@ class UserFefuRepository @Inject constructor(
 ): UserDataRepository {
 
     override suspend fun getUserData(token:String): DataUserDataModel {
-        return api.getUserData(mapOf("token" to token))
+        try {
+            return api.getUserData(mapOf("token" to token))
+        }
+        catch (cause:Throwable){
+            val errorText: String = when (cause) {
+                is HttpException -> {
+                    val result =
+                        JSONObject(cause.response()?.errorBody()?.string().toString()).toMap()
+                    result["detail"].toString()
+                }
+
+                is NullPointerException -> {
+                    "User token is null"
+                }
+
+                else -> "Unknown error"
+            }
+            throw Exception(errorText)
+        }
     }
 
     override suspend fun editUserData(token:String): Map<String, String> {
--- features/main-impl/src/main/java/com/example/main_impl/domain/use_cases/UserShortDataUseCase.kt
+++ features/main-impl/src/main/java/com/example/main_impl/domain/use_cases/UserShortDataUseCase.kt
@@ -25,17 +25,3 @@ class UserShortDataUseCase @Inject constructor(
         }
     }
 }
-
-//TODO DELETE
-
-//when (cause) {
-//    is HttpException -> {
-//        val result = JSONObject(cause.response()?.errorBody()?.string().toString()).toMap()
-//        emit(Resource.Error(result["detail"].toString()))
-//    }
-//    is NullPointerException -> {
-//        val errorText = "User token is null"
-//        emit(Resource.Error(errorText))
-//    }
-//    else -> emit(Resource.Error("Unknown error"))
-//}
