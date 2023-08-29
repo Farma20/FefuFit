22a26faeb5fcc72dde8b1d6233bcdfc8e01c83d5
Farma20 <kustov.ius@students.dvfu.ru>
2023-08-29 14:08:49 +1000

    create exceptions handler in ServicesFefuRepository

--- data/remote/src/main/java/com/example/remote/repositories/ServicesFefuRepository.kt
+++ data/remote/src/main/java/com/example/remote/repositories/ServicesFefuRepository.kt
@@ -3,13 +3,33 @@ package com.example.fefufit.data.remote.repositories
 import com.example.fefufit.data.remote.api.FefuFitApi
 import com.example.fefufit.data.remote.models.services_data_models.DataUserServicesDataModel
 import com.example.remote.ServicesDataRepository
+import com.example.remote.utils.toMap
+import org.json.JSONObject
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class ServicesFefuRepository @Inject constructor(
     private val api: FefuFitApi
 ): ServicesDataRepository {
     override suspend fun getUserServices(token:String): DataUserServicesDataModel {
-        return api.getActiveUserPlans(mapOf("token" to token))
-    }
+        try {
+            return api.getActiveUserPlans(mapOf("token" to token))
+        }
+        catch (cause:Throwable){
+            val errorText: String = when (cause) {
+                is HttpException -> {
+                    val result =
+                        JSONObject(cause.response()?.errorBody()?.string().toString()).toMap()
+                    result["detail"].toString()
+                }
 
+                is NullPointerException -> {
+                    "User token is null"
+                }
+
+                else -> "Unknown error"
+            }
+            throw Exception(errorText)
+        }
+    }
 }
