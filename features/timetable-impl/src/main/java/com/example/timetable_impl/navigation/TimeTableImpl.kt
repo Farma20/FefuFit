2368a695c068c6ddc7137df8db63ace07f5495a7
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-12 17:01:48 +1000

    create timetable-impl module and create TimeTableImpl class

---
+++ features/timetable-impl/src/main/java/com/example/timetable_impl/navigation/TimeTableImpl.kt
@@ -0,0 +1,48 @@
+package com.example.timetable_impl.navigation
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.example.core.theme.FefuFitTheme
+import com.example.timetable_api.TimeTableApi
+
+private const val GRAPH_ROUTE = "timeTablePageGraph"
+private const val TIMETABLE_ROUTE = "timeTablePageRoute"
+class TimeTableImpl():TimeTableApi {
+    override val route: String = GRAPH_ROUTE
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun registerGraph(
+        navGraphBuilder: NavGraphBuilder,
+        navController: NavHostController,
+        modifier: Modifier
+    ) {
+        navGraphBuilder.navigation(
+            route = route,
+            startDestination = TIMETABLE_ROUTE
+        ){
+            composable(TIMETABLE_ROUTE){
+                Scaffold(
+                    containerColor = FefuFitTheme.color.mainAppColors.appBackgroundColor
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(text = "Расписание в разработке")
+                    }
+                }
+            }
+        }
+    }
+}
