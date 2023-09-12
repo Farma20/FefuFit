2fdce01470b6049851895615cd705729530568c4
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-12 13:35:03 +1000

    add test navigation page

--- app/src/main/java/com/example/fefufit/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/fefufit/navigation/AppNavGraph.kt
@@ -1,13 +1,25 @@
 package com.example.fefufit.navigation
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.example.core.theme.FefuFitTheme
 import com.example.feature_api.register
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppNavGraph(
     modifier: Modifier = Modifier,
@@ -16,8 +28,8 @@ fun AppNavGraph(
 ) {
     NavHost(
         navController = navController,
-//        startDestination = featureApiHolder.mainScreen.route,
-        startDestination = featureApiHolder.singInScreen.route,
+        startDestination = BottomTabs.Main.route,
+//        startDestination = featureApiHolder.singInScreen.route,
         modifier = Modifier.background(FefuFitTheme.color.mainAppColors.appBackgroundColor)
     ){
         register(
@@ -25,10 +37,52 @@ fun AppNavGraph(
             navController,
             modifier
         )
-        register(
-            featureApiHolder.mainScreen,
-            navController,
-            modifier
-        )
+
+        navigation(
+            route = BottomTabs.Main.route,
+            startDestination = featureApiHolder.mainScreen.route
+        ){
+            register(
+                featureApiHolder.mainScreen,
+                navController,
+                modifier
+            )
+        }
+
+        navigation(
+            route = BottomTabs.Calendar.route,
+            startDestination ="calendar"
+        ){
+            composable("calendar"){
+                Scaffold(
+                    containerColor = FefuFitTheme.color.mainAppColors.appBackgroundColor
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(text = "Расписание в разработке")
+                    }
+                }
+            }
+        }
+
+        navigation(
+            route = BottomTabs.Services.route,
+            startDestination = "services"
+        ){
+            composable("services"){
+                Scaffold(
+                    containerColor = FefuFitTheme.color.mainAppColors.appBackgroundColor
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(text = "Абонементы в разработке")
+                    }
+                }
+            }
+        }
     }
 }
