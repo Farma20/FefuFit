4d681907cd8be11f6dba4ea2887f3bdab3a20c4f
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-14 14:30:14 +1000

    change colors in calendar

--- features/timetable-impl/src/main/java/com/example/timetable_impl/presentation/timetable_screen/TimeTableScreen.kt
+++ features/timetable-impl/src/main/java/com/example/timetable_impl/presentation/timetable_screen/TimeTableScreen.kt
@@ -4,21 +4,28 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -58,7 +65,7 @@ fun TimeTableScreen(
 }
 
 @Composable
-fun SingleRowCalendar() {
+private fun SingleRowCalendar() {
     val currentDate = remember {LocalDate.now()}
     val currentMonth = remember {YearMonth.now()}
     val startMonth = remember {currentMonth.minusMonths(100).atStartOfMonth()}
@@ -74,15 +81,21 @@ fun SingleRowCalendar() {
 
     WeekCalendar(
         state = calendarState,
-        dayContent = {Day(it)}
+        dayContent = {Day(it, currentDate)}
     ) {
 
     }
 }
 
 @Composable
-fun Day(dayData: WeekDay) {
-    IconButton(
+private fun Day(dayData: WeekDay, currentDate: LocalDate) {
+    TextButton(
+        modifier = Modifier.size(42.dp),
+        colors = ButtonDefaults.textButtonColors(
+            containerColor = if (dayData.date == currentDate) FefuFitTheme.color.elementsColor.elementColor else Color.Transparent,
+            contentColor = if (dayData.date == currentDate) FefuFitTheme.color.elementsColor.onElementsColor else FefuFitTheme.color.textColor.mainTextColor
+        ),
+        contentPadding = PaddingValues(0.dp),
         onClick = {}
     ) {
         Text(text = dayData.date.dayOfMonth.toString())
