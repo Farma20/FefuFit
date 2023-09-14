74578be5b4a2aceb65d347230f49bf684e6ef4f8
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-14 14:08:33 +1000

    start creating week calendar in timetable-module

--- features/timetable-impl/src/main/java/com/example/timetable_impl/presentation/timetable_screen/TimeTableScreen.kt
+++ features/timetable-impl/src/main/java/com/example/timetable_impl/presentation/timetable_screen/TimeTableScreen.kt
@@ -2,6 +2,7 @@ package com.example.timetable_impl.presentation.timetable_screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +16,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -23,6 +25,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.core.theme.FefuFitTheme
 import com.example.timetable_impl.R
+import com.kizitonwose.calendar.compose.WeekCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
+import com.kizitonwose.calendar.core.WeekDay
+import com.kizitonwose.calendar.core.atStartOfMonth
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import java.time.LocalDate
+import java.time.YearMonth
+import kotlin.time.Duration.Companion.days
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -40,8 +51,41 @@ fun TimeTableScreen(
         ) {
             Spacer(modifier = Modifier.height(18.dp))
             TopBar(modifier.fillMaxWidth())
+            SingleRowCalendar()
             Spacer(modifier = modifier)
         }
+    }
+}
+
+@Composable
+fun SingleRowCalendar() {
+    val currentDate = remember {LocalDate.now()}
+    val currentMonth = remember {YearMonth.now()}
+    val startMonth = remember {currentMonth.minusMonths(100).atStartOfMonth()}
+    val endMonth = remember {currentMonth.plusMonths(100).atEndOfMonth()}
+    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() }
+
+    val calendarState = rememberWeekCalendarState(
+        startDate = startMonth,
+        endDate = endMonth,
+        firstVisibleWeekDate = currentDate,
+        firstDayOfWeek = firstDayOfWeek
+    )
+
+    WeekCalendar(
+        state = calendarState,
+        dayContent = {Day(it)}
+    ) {
+
+    }
+}
+
+@Composable
+fun Day(dayData: WeekDay) {
+    IconButton(
+        onClick = {}
+    ) {
+        Text(text = dayData.date.dayOfMonth.toString())
     }
 }
 
