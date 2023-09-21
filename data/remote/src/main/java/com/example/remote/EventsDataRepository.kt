c9312f728b1329fc00f7287ecd52ef4de1276c48
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-21 13:45:15 +1000

    create getEvents function in EventsFefuRepository

--- data/remote/src/main/java/com/example/remote/EventsDataRepository.kt
+++ data/remote/src/main/java/com/example/remote/EventsDataRepository.kt
@@ -1,7 +1,10 @@
 package com.example.remote
 
 import com.example.fefufit.data.remote.models.events_data_models.DataUserBookingDataModel
+import com.example.remote.models.events_data_models.DataEventDataModel
 
 interface EventsDataRepository {
     suspend fun getAllUserBookings(token:String): DataUserBookingDataModel
+
+    suspend fun getEvents(token: String): DataEventDataModel
 }
--- data/remote/src/main/java/com/example/remote/repositories/EventsFefuRepository.kt
+++ data/remote/src/main/java/com/example/remote/repositories/EventsFefuRepository.kt
@@ -3,6 +3,7 @@ package com.example.fefufit.data.remote.repositories
 import com.example.fefufit.data.remote.api.FefuFitApi
 import com.example.fefufit.data.remote.models.events_data_models.DataUserBookingDataModel
 import com.example.remote.EventsDataRepository
+import com.example.remote.models.events_data_models.DataEventDataModel
 import com.example.remote.utils.toMap
 import org.json.JSONObject
 import retrofit2.HttpException
@@ -15,6 +16,27 @@ class EventsFefuRepository @Inject constructor(
     override suspend fun getAllUserBookings(token:String): DataUserBookingDataModel {
         try {
             return api.getUserBookings(mapOf("token" to token))
+        }catch (cause:Throwable){
+            val errorText: String = when (cause) {
+                is HttpException -> {
+                    val result =
+                        JSONObject(cause.response()?.errorBody()?.string().toString()).toMap()
+                    result["detail"].toString()
+                }
+
+                is NullPointerException -> {
+                    "User token is null"
+                }
+
+                else -> "Unknown error"
+            }
+            throw Exception(errorText)
+        }
+    }
+
+    override suspend fun getEvents(token: String): DataEventDataModel {
+        try {
+            return api.getEvents(token)
         }catch (cause:Throwable){
             val errorText: String = when (cause) {
                 is HttpException -> {
