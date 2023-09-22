5a640c8b049db4a3464e7a84d4ac228c1cc518be
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-22 15:12:24 +1000

    create AddEventsUseCase

---
+++ features/timetable-impl/src/main/java/com/example/timetable_impl/domain/models/EventActionResponse.kt
@@ -0,0 +1,5 @@
+package com.example.timetable_impl.domain.models
+
+data class EventActionResponse(
+    val details: String
+)
---
+++ features/timetable-impl/src/main/java/com/example/timetable_impl/domain/use_cases/AddEventsUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.timetable_impl.domain.use_cases
+
+import com.example.common.Resource
+import com.example.timetable_impl.domain.models.EventActionResponse
+import com.example.timetable_impl.domain.repositories.TimeTableEventsFeatureRepository
+import com.example.timetable_impl.domain.repositories.TimeTableMetaDataRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class AddEventsUseCase @Inject constructor(
+    private val eventsRepository: TimeTableEventsFeatureRepository,
+    private val metaDataRepository: TimeTableMetaDataRepository
+) {
+    operator fun invoke(eventId: Int): Flow<Resource<EventActionResponse>> = flow {
+        try {
+            emit(Resource.Loading())
+            val token = metaDataRepository.getUserTokenMetaData()
+            val result = eventsRepository.addEvents(token, eventId)
+            val eventResponse = EventActionResponse(result["details"]!!)
+            emit(Resource.Success(eventResponse))
+        }
+        catch (e:Exception){
+            val eventResponse = EventActionResponse(e.message.toString())
+            emit(Resource.Error("error", eventResponse))
+        }
+    }
+}
