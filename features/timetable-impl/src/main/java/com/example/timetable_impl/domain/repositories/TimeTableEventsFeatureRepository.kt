f9f9872db557aabe2fd553f45d179fc5207fda92
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-22 14:58:15 +1000

    add add/cancel events in repository inteface time-table module

--- features/timetable-impl/src/main/java/com/example/timetable_impl/domain/repositories/TimeTableEventsFeatureRepository.kt
+++ features/timetable-impl/src/main/java/com/example/timetable_impl/domain/repositories/TimeTableEventsFeatureRepository.kt
@@ -4,4 +4,8 @@ import com.example.timetable_impl.domain.models.EventDataModel
 
 interface TimeTableEventsFeatureRepository {
     suspend fun getEvents(token:String):EventDataModel
+
+    suspend fun addEvents(token: String, eventId:Int): Map<String, String>
+
+    suspend fun cancelEvents(token: String, eventId:Int): Map<String, String>
 }
